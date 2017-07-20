f7df5d0ed3c358957ec3232ddf4862678f4b5d91
Nicole <nicole.tempas@gmail.com>
2017-07-20 17:08:14 +0000

    Add jQuery Library

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -16,6 +16,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+
 
 app.use('/', index);
 app.use('/users', users);
