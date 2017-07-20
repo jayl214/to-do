f5e86ab68a6e8314b0e3bb48699b05baf37e4f90
keano voelkl <keanov@gmail.com>
2017-07-20 18:08:47 +0000

    restructred project beginning of amazon search

---
+++ public/scripts/search.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+var amazon = require('amazon-product-api');
+
+var client = amazon.createClient({
+  awsId: "AKIAIE6ZKACQTLVXRPIQ",
+  awsSecret: "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",
+  awsTag: "wat0a-20"
+});
+
+$('#amazonsearch').on('submit', function(event) {
+  let searchTerms =
+})
+
+});
